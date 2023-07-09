30a11c51aa3e2460339d42ef87f260f1708b63bc
Christine Bui <Christine.Bui@student.csulb.edu>
2023-07-09 12:36:01 -0700

    created more fragments and a bottom navigation menu

--- app/src/main/java/com/flip/flashcards/MainActivity.kt
+++ app/src/main/java/com/flip/flashcards/MainActivity.kt
@@ -2,10 +2,84 @@ package com.flip.flashcards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    //Create our four fragments object
+    lateinit var homeFragment: HomeFragment
+    lateinit var searchFragment: SearchFragment
+    lateinit var newFragment: NewFragment
+    lateinit var favFragment: FavFragment
+    lateinit var shareFragment: SharingFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //now let's create our framelayout and bottomnav variables
+        var bottomnav = findViewById<BottomNavigationView>(R.id.BottomNavMenu)
+        var frame = findViewById<FrameLayout>(R.id.frameLayout)
+        //Now let's the deffault Fragment
+        homeFragment = HomeFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frameLayout,homeFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+        //now we will need to create our different fragemnts
+        //Now let's add the menu evenet listener
+        bottomnav.setOnNavigationItemSelectedListener { item ->
+            //we will select each menu item and add an event when it's selected
+            when(item.itemId){
+                R.id.home -> {
+                    homeFragment = HomeFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,homeFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+                R.id.search -> {
+                    searchFragment = SearchFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,searchFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.new_set -> {
+                    newFragment = NewFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,newFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.favorite -> {
+                    favFragment = FavFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,favFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+
+                R.id.share -> {
+                    shareFragment = SharingFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frameLayout,shareFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                }
+            }
+
+            true
+        }
+        //Now let's Run our App
     }
 }
