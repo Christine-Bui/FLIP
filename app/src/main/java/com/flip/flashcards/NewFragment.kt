2210a4948338d8ef95c0253fda56a2eb202d0c44
David Arturo Penate <david.penate@student.csulb.edu>
2023-07-13 23:08:24 -0700

    Update strings.mxl, new fragment kt and xml, initialized new card adapter

--- app/src/main/java/com/flip/flashcards/NewFragment.kt
+++ app/src/main/java/com/flip/flashcards/NewFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +31,18 @@ class NewFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_new, container, false)
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.card_set_view)
+        /*val adapter = newCardAdapter();
+        recyclerView.adapter = adapter;
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        */
+
+
+
+        return rootView
     }
 
     companion object {
