772cb9a2e4ce4ea0a8bf1cc51e7718decc1cb687
David Arturo Penate <david.penate@student.csulb.edu>
2023-07-20 08:09:30 -0700

    update newfragment

--- app/src/main/java/com/flip/flashcards/NewFragment.kt
+++ app/src/main/java/com/flip/flashcards/NewFragment.kt
@@ -23,12 +23,27 @@ class NewFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+    }
+
+
+    fun insertCard(view: View){
+
+    }
+
+    fun removeCard(view: View){
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
