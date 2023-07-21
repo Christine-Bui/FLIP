3d4215b164331b51076b90ad5f0fa21e7a8c8413
David Arturo Penate <david.penate@student.csulb.edu>
2023-07-20 17:57:04 -0700

    Create card set UI fragment
    the card set layout is to be listed in the home fragment.

--- app/src/main/java/com/flip/flashcards/HomeFragment.kt
+++ app/src/main/java/com/flip/flashcards/HomeFragment.kt
@@ -29,18 +29,9 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //var newButton: Button = findViewById(R.id.addItem)
     }
 
-    /*override fun onCreateView(
 
-
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }*/
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,7 +43,7 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        //when button is clicked, new_set_name fragment is shown.
         binding.newSet.setOnClickListener {
             // Your button click logic here
 
@@ -62,30 +53,7 @@ class HomeFragment : Fragment() {
         }
     private fun showDialog() {
         val dialog = Dialog(requireContext())
-
         dialog.setContentView(R.layout.new_set_name)
         dialog.show()
     }
 }
-
-
-
- /*   companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }*/
